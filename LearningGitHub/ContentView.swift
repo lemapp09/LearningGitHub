ad2237f06959c34b212d4d721c4cd6ed8111ed55
Lem Apperson <lemapp09@gmail.com>
2021-05-06 09:05:27 -0400

    Refacted Modal View to a separate file

--- LearningGitHub/ContentView.swift
+++ LearningGitHub/ContentView.swift
@@ -19,8 +19,9 @@ struct ContentView: View {
             Button("Show Modal") {
                 self.isPresented = true
             }.sheet(isPresented: $isPresented) {
-                Text("This is the modal screen")
+                ModalView()
             }
+            .navigationBarTitle("Xcode and git")
         }
     }
 }
---
+++ LearningGitHub/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  LearningGitHub
+//
+//  Created by Lemuel R Apperson III on 5/6/21.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the Modal View!")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
